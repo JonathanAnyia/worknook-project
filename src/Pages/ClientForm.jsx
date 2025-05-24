7ee8dee86e106373f9bc449307803fa19b182039
Donald-Source2023 <donalddyusuf@gmail.com>
2025-05-24 07:41:30 -0700

    auth

--- src/Pages/ClientForm.jsx
+++ src/Pages/ClientForm.jsx
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import ArtisanBg from "../assets/ArtisanBg.png";
 
 const ClientForm = () => {
   const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    phoneNumber: '',
-    sex: '',
-    maritalStatus: '',
-    address: '',
-    state: '',
-    city: '',
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    sex: "",
+    maritalStatus: "",
+    address: "",
+    state: "",
+    lga: "",
   });
   const [profilePicture, setProfilePicture] = useState(null);
+  const [states, setStates] = useState([]);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
+  const fetchStates = async () => {
+    try {
+      const response = await fetch("https://nga-states-lga.onrender.com/fetch");
+      // console.log(response);
+
+      const data = await response.json();
+      setStates(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching states:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchStates();
+  }, []);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -31,34 +49,60 @@ const ClientForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
-    console.log('Profile picture:', profilePicture);
+    console.log("Form submitted:", formData);
+    console.log("Profile picture:", profilePicture);
     // Handle form submission logic here
   };
 
-  const inputStyle = "mt-1 block w-full px-0.5 border-0 border-b-2 border-gray-300 bg-transparent focus:outline-none";
+  const inputStyle =
+    "mt-1 block w-full px-0.5 border-0 border-b-2 border-gray-300 bg-transparent focus:outline-none";
 
   return (
-    <div className="flex items-center justify-center min-h-screen p-4 relative" style={{ backgroundImage: `url("${ArtisanBg}")` }}>
-        <div className="absolute inset-0 bg-blue-100 bg-opacity-30"></div>
+    <div
+      className="flex items-center justify-center min-h-screen p-4 relative"
+      style={{ backgroundImage: `url("${ArtisanBg}")` }}
+    >
+      <div className="absolute inset-0 bg-blue-100 bg-opacity-30"></div>
       <div className="max-w-2xl w-full space-y-8 z-10">
         <div>
           <h2 className="mt-6 text-center text-xl md:text-2xl font-bold">
             Applicants Profile
           </h2>
         </div>
-        <form className="mt-8 space-y-6 shadow-md rounded px-8 pt-6 pb-8 mb-4 rounded-br-[2.5rem]" onSubmit={handleSubmit}>
+        <form
+          className="mt-8 space-y-6 shadow-md rounded px-8 pt-6 pb-8 mb-4 rounded-br-[2.5rem]"
+          onSubmit={handleSubmit}
+        >
           <div className="flex justify-end mb-4">
             <div className="relative w-32 h-32">
               <img
-                src={profilePicture || '/api/placeholder/128/128'}
+                src={profilePicture || "/api/placeholder/128/128"}
                 alt="Profile"
                 className="w-full h-full object-cover rounded-lg"
               />
-              <label htmlFor="profile-picture" className="absolute bottom-0 right-0 bg-white rounded-full p-2 cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+              <label
+                htmlFor="profile-picture"
+                className="absolute bottom-0 right-0 bg-white rounded-full p-2 cursor-pointer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="h-6 w-6 text-gray-600"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
                 </svg>
               </label>
               <input
@@ -72,8 +116,11 @@ const ClientForm = () => {
           </div>
           <div className="space-y-4 sm:w-[70%]">
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-              <div  className='sm:col-span-2 sm:w-[70%]'>
-                <label htmlFor="fullName" className="block text-sm font-bold text-gray-700">
+              <div className="sm:col-span-2 sm:w-[70%]">
+                <label
+                  htmlFor="fullName"
+                  className="block text-sm font-bold text-gray-700"
+                >
                   Full Name
                 </label>
                 <input
@@ -87,7 +134,10 @@ const ClientForm = () => {
                 />
               </div>
               <div>
-                <label htmlFor="email" className="block text-sm font-bold text-gray-700">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-bold text-gray-700"
+                >
                   Email
                 </label>
                 <input
@@ -101,7 +151,10 @@ const ClientForm = () => {
                 />
               </div>
               <div>
-                <label htmlFor="phoneNumber" className="block text-sm font-bold text-gray-700">
+                <label
+                  htmlFor="phoneNumber"
+                  className="block text-sm font-bold text-gray-700"
+                >
                   Phone number
                 </label>
                 <input
@@ -114,36 +167,12 @@ const ClientForm = () => {
                   onChange={handleChange}
                 />
               </div>
-              <div>
-                <label htmlFor="sex" className="block text-sm font-bold text-gray-700">
-                  Sex
-                </label>
-                <input
-                  id="sex"
-                  name="sex"
-                  type="text"
-                  required
-                  className={inputStyle}
-                  value={formData.sex}
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <label htmlFor="maritalStatus" className="block text-sm font-bold text-gray-700">
-                  Marital Status
-                </label>
-                <input
-                  id="maritalStatus"
-                  name="maritalStatus"
-                  type="text"
-                  required
-                  className={inputStyle}
-                  value={formData.maritalStatus}
-                  onChange={handleChange}
-                />
-              </div>
+
               <div className="sm:col-span-2 sm:w-[70%]">
-                <label htmlFor="address" className="block text-sm font-bold text-gray-700">
+                <label
+                  htmlFor="address"
+                  className="block text-sm font-bold text-gray-700"
+                >
                   Address
                 </label>
                 <input
@@ -157,21 +186,34 @@ const ClientForm = () => {
                 />
               </div>
               <div>
-                <label htmlFor="state" className="block text-sm font-bold text-gray-700">
+                <label
+                  htmlFor="state"
+                  className="block text-sm font-bold text-gray-700"
+                >
                   State
                 </label>
-                <input
-                  id="state"
-                  name="state"
-                  type="text"
-                  required
+                <select
+                  onChange={handleChange}
                   className={inputStyle}
                   value={formData.state}
-                  onChange={handleChange}
-                />
+                  name=""
+                  id=""
+                >
+                  {states.map((item, idx) => {
+                    return (
+                      <option className="w-full" key={idx} value={item.state}>
+                        {item}
+                      </option>
+                    );
+                  })}
+                  <option value=""></option>
+                </select>
               </div>
               <div>
-                <label htmlFor="city" className="block text-sm font-bold text-gray-700">
+                <label
+                  htmlFor="city"
+                  className="block text-sm font-bold text-gray-700"
+                >
                   City
                 </label>
                 <input
@@ -187,7 +229,7 @@ const ClientForm = () => {
             </div>
           </div>
 
-          <div className='flex justify-end'>
+          <div className="flex justify-end">
             <button
               type="submit"
               className="w-fit flex justify-center py-2 px-5 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-sky-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
@@ -202,4 +244,3 @@ const ClientForm = () => {
 };
 
 export default ClientForm;
-
--- src/Pages/WorkerDetails.jsx
+++ src/Pages/WorkerDetails.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import RatingDisplay from '../Components/RatingDisplay';
-import RatingInput from '../Components/RatingInput';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import RatingDisplay from "../Components/RatingDisplay";
+import RatingInput from "../Components/RatingInput";
 
 const WorkerDetails = () => {
   const { id } = useParams();
@@ -11,10 +11,12 @@ const WorkerDetails = () => {
   useEffect(() => {
     const fetchWorker = async () => {
       try {
-        const res = await axios.get(`http://localhost:2345/api/users/worker/${id}`);
+        const res = await axios.get(
+          `http://localhost:2345/api/users/worker/${id}`
+        );
         setWorker(res.data);
       } catch (err) {
-        console.error('Error fetching worker:', err);
+        console.error("Error fetching worker:", err);
       }
     };
 
@@ -30,9 +32,12 @@ const WorkerDetails = () => {
       <h1 className="text-3xl font-bold mb-2">{worker.name}</h1>
       <p className="text-blue-600 font-semibold mb-2">{worker.serviceType}</p>
       <p className="text-gray-700 mb-4">{worker.bio}</p>
-
+    
       {/* Show rating */}
-      <RatingDisplay rating={worker.rating || 0} totalRatings={worker.totalRatings || 0} />
+      <RatingDisplay
+        rating={worker.rating || 0}
+        totalRatings={worker.totalRatings || 0}
+      />
 
       {/* Allow users to submit a rating */}
       <div className="mt-8">
