2d30f0bab43034ed068d1930902497b6dc79fc67
Donald-Source2023 <donalddyusuf@gmail.com>
2025-06-07 11:22:52 -0700

    auth

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
         <div>
           <input
             onChange={handleSearch}
-            className="focus:outline-0 bg-white/80 border-gray-300 w-1/3 my-3 rounded-full py-4s px-8  outline-0"
+            className="focus:outline-0 bg-white/80 border-gray-300 w-1/3 my-3 rounded-full py-4 px-8  outline-0"
             placeholder="Search"
             type="text"
           />
